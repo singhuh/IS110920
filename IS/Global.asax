eadfcb3b7a6effebd94dd0f2943d93ed4d402a5d
Umesh Singh <singhuh@ucmail.uc.edu>
2020-11-09 16:04:19 -0500

    Add project files.

---
+++ IS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IS.MvcApplication" Language="C#" %>
---
+++ IS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IS.Startup))]
+namespace IS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
